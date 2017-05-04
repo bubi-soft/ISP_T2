9a28d9e46c4fb3e9d53d6b0ac09478c2f1017940
bubi-soft <ionutflorea.bubisoft@gmail.com>
2017-05-04 14:05:58 +0300

    asdasd

---
+++ src/TestPackage/TestReteaAutobuz.java
@@ -0,0 +1,34 @@
+package TestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestReteaAutobuz {
+
+	@Test
+	public void testModStatie() {
+		fail("Not yet implemented");//TODO
+	}
+
+	@Test
+	public void testDelStatie() {
+		fail("Not yet implemented");//TODO
+	}
+
+	@Test
+	public void testAddLinieIntArrayListOfStatie() {
+		fail("Not yet implemented");//TODO
+	}
+
+	@Test
+	public void testGetLinie() {
+		fail("Not yet implemented");//TODO
+	}
+
+	@Test
+	public void testDelLinie() {
+		fail("Not yet implemented");//TODO
+	}
+
+}
